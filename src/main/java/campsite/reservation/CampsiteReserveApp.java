b6b3a311bfe7751ffb5eab768562685aeacaa37a
Dmitiry Fingerman <dmitriy.fingerman@gmail.com>
2020-10-10 22:37:07 -0400

    Added liquibase config for initial H2 schema creation.

---
+++ src/main/java/campsite/reservation/CampsiteReserveApp.java
@@ -0,0 +1,12 @@
+package campsite.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsiteReserveApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampsiteReserveApp.class, args);
+	}
+}
