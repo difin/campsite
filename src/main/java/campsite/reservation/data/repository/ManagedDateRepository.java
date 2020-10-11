23526b7b75b6e92196eaf92badba812c26f0480f
Dmitiry Fingerman <dmitriy.fingerman@gmail.com>
2020-10-11 11:28:12 -0400

    Externalized number of camping spots property.

--- src/main/java/campsite/reservation/data/repository/ManagedDateRepository.java
+++ src/main/java/campsite/reservation/data/repository/ManagedDateRepository.java
@@ -3,14 +3,16 @@ package campsite.reservation.data.repository;
 import campsite.reservation.data.entity.ManagedDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ManagedDateRepository extends JpaRepository<ManagedDate, Integer> {
 
     @Query("select md                       " +
            "from ManagedDate md             " +
-           "where md.reservedDates.size < 3 "
+           "where md.reservedDates.size < :spotsNum "
     )
-    List<ManagedDate> getAvailableDates();
+    List<ManagedDate> getAvailableDates(int spotsNum);
 }
--- src/main/java/campsite/reservation/service/AvailabilityService.java
+++ src/main/java/campsite/reservation/service/AvailabilityService.java
@@ -2,6 +2,7 @@ package campsite.reservation.service;
 
 import campsite.reservation.data.repository.ManagedDateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,13 +13,17 @@ import java.util.stream.Collectors;
 public class AvailabilityService {
 
     private final ManagedDateRepository managedDateRepository;
+    private int spotsNum;
 
     @Autowired
-    public AvailabilityService(ManagedDateRepository managedDateRepository) {
+    public AvailabilityService(ManagedDateRepository managedDateRepository,
+                               @Value("${app.spots-num}") int spotsNum) {
+
         this.managedDateRepository = managedDateRepository;
+        this.spotsNum = spotsNum;
     }
 
    public List<LocalDate> getAvailableDates(){
-       return managedDateRepository.getAvailableDates().stream().map(m -> m.getDate()).collect(Collectors.toList());
+       return managedDateRepository.getAvailableDates(spotsNum).stream().map(m -> m.getDate()).collect(Collectors.toList());
     }
 }
