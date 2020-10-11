32dc5b82edfc90b2e409b1bb6e85d7465324d8e1
Dmitiry Fingerman <dmitriy.fingerman@gmail.com>
2020-10-11 18:58:55 -0400

    Replaced hardcoded getter/setter with lombok.

--- src/main/java/campsite/reservation/data/entity/ManagedDate.java
+++ src/main/java/campsite/reservation/data/entity/ManagedDate.java
@@ -4,42 +4,25 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+import lombok.*;
+
 @Entity
 @Table(name = "MANAGED_DATE")
 public class ManagedDate {
 
 	@Id
+	@Getter
+	@Setter
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "managed_date_seq")
 	@SequenceGenerator(name = "managed_date_seq", sequenceName = "managed_date_id_seq", allocationSize = 1)
 	private int id;
 
+	@Getter
+	@Setter
 	@Column(nullable = false)
 	private LocalDate date;
 
+	@Getter
 	@OneToMany(mappedBy = "managedDate", fetch = FetchType.LAZY)
 	List<ReservedDate> reservedDates;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
-	public List<ReservedDate> getReservedDates() {
-		return reservedDates;
-	}
-
-	public void setReservedDates(List<ReservedDate> reservedDates) {
-		this.reservedDates = reservedDates;
-	}
 }
