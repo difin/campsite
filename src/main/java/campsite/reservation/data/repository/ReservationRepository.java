f33c1ae3eb10b0b222782d50f0347853a2eb87ed
Dmitiry Fingerman <dmitriy.fingerman@gmail.com>
2020-10-13 10:08:01 -0400

    Added database locking when cancelling reservation.

--- src/main/java/campsite/reservation/data/repository/ReservationRepository.java
+++ src/main/java/campsite/reservation/data/repository/ReservationRepository.java
@@ -2,12 +2,16 @@ package campsite.reservation.data.repository;
 
 import campsite.reservation.data.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select r                                " +
             "from Reservation r                     " +
             "join FETCH r.reservedDates             " +
